962f0d272b7b6e97581d06fc991e5b1bc3795630
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-12-13 22:49:18 +0600

    Initial setup for the application

---
+++ app/(store)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello world 1234</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+function Header() {
+  return <div>Header</div>;
+}
+
+export default Header;

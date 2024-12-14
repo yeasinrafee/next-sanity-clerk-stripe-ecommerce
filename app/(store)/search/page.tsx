d6a3033b3537c39b29ac2a42c4d87a548784ad40
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-12-14 14:40:43 +0600

    Order type for sanity implemented

---
+++ app/(store)/search/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+async function SearchPage({
+  searchParams,
+}: {
+  searchParams: { query: string };
+}) {
+  const { query } = await searchParams;
+  return <div>Search Page for {query}</div>;
+}
+
+export default SearchPage;
